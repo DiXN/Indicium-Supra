07160b020d54c8845610df4932f0a1a79327c590
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-07-16 03:45:03 +0200

    Refactoring.

--- src/Indicium-Supra/dllmain.cpp
+++ src/Indicium-Supra/dllmain.cpp
@@ -17,41 +17,44 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
 
 	switch (dwReason)
 	{
-	case DLL_PROCESS_ATTACH:
+		case DLL_PROCESS_ATTACH:
 		{
 			logging::add_common_attributes();
 
 			logging::add_file_log
-				(
-					keywords::file_name = "Indicium-Supra.log",
-					keywords::auto_flush = true,
-					keywords::format = (
-						expr::stream
-						<< expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%d.%m.%Y - %H:%M:%S")
-						<< ": [" << logging::trivial::severity
-						<< "] " << expr::smessage
-					)
-				);
+			(
+				keywords::file_name = "Indicium-Supra.log",
+				keywords::auto_flush = true,
+				keywords::format = (
+					expr::stream
+					<< expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%d.%m.%Y - %H:%M:%S")
+					<< ": [" << logging::trivial::severity
+					<< "] " << expr::smessage
+				)
+			);
 
 			logging::core::get()->set_filter
-				(
-					logging::trivial::severity >= logging::trivial::info
-				);
+			(
+				logging::trivial::severity >= logging::trivial::info
+			);
 
-			BOOST_LOG_TRIVIAL(info) << "Library loaded, attempting to launch main thread";
+			if (GetModuleHandle("d3d9.dll") || GetModuleHandle("d3d10.dll") || GetModuleHandle("d3d11.dll")) {
+				BOOST_LOG_TRIVIAL(info) << "Library loaded, attempting to launch main thread";
 
-			auto hMain = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(initGame), nullptr, 0, nullptr);
+				auto hMain = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(initGame), nullptr, 0, nullptr);
 
-			if (hMain == nullptr)
-			{
-				BOOST_LOG_TRIVIAL(error) << "Couldn't create main thread, library unusable";
-				return FALSE;
+				if (hMain == nullptr)
+				{
+					BOOST_LOG_TRIVIAL(error) << "Couldn't create main thread, library unusable";
+					return FALSE;
+				}
+
+				BOOST_LOG_TRIVIAL(info) << "Main thread created successfully";
 			}
 
-			BOOST_LOG_TRIVIAL(info) << "Main thread created successfully";
 			return TRUE;
 		}
-	case DLL_PROCESS_DETACH:
+		case DLL_PROCESS_DETACH:
 		{
 			BOOST_LOG_TRIVIAL(info) << "Shutting down hooks...";
 
